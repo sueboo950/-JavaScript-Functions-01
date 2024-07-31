30a87f49ae1f40588304c0a8c0ec1bebd8d92af0
sophie950 <sueboo950950@gmail.com>
2024-07-31 01:09:51 -0500

    completed song with extracted refrains

---
+++ index.js
@@ -0,0 +1,34 @@
+function refrain() {
+    console.log("Glory to Your name");
+    console.log("Glory to Your name");
+    console.log("Glory to Your name");
+    console.log("Forever You are the same");
+    console.log("We worship and adore You");
+    console.log("We bow ourselves before You");
+    console.log("Giving You the glory");
+    console.log("That is due Your name");
+}
+
+function refrain2() {
+    console.log("We magnify Your name");
+    console.log("Glorify Your name");
+    console.log("Oh God");
+}
+
+refrain();
+
+refrain();
+
+refrain();
+
+refrain2();
+
+refrain2();
+
+refrain2();
+
+refrain2();
+
+refrain2();
+
+refrain2();
