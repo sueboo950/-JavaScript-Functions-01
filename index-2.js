8b74f28f3f985206b8422c69684198004cec1fcc
sophie950 <sueboo950950@gmail.com>
2024-08-02 15:32:28 -0500

    completed day 2 song

---
+++ index-2.js
@@ -0,0 +1,47 @@
+function refrain(){
+    let words = "";
+    words += "We magnify Your name\n";
+    words += "Glorify Your name\n";
+    words += "Oh God\n";
+    return words;
+}
+
+
+function song(){
+    let r;
+
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Forever You are the same\n");
+    console.log("We worship and adore You\n");
+    console.log("We bow ourselves before You\n");
+    console.log("Giving You the glory\n");
+    console.log("That is due Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Forever You are the same\n");
+    console.log("We worship and adore You\n");
+    console.log("We bow ourselves before You\n");
+    console.log("Giving You the glory\n");
+    console.log("That is due Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Glory to Your name\n");
+    console.log("Forever You are the same\n");
+    console.log("We worship and adore You\n");
+    console.log("We bow ourselves before You\n");
+    console.log("Giving You the glory\n");
+    console.log("That is due Your name\n");
+    r = refrain();
+    console.log(r);
+    console.log(r);
+    console.log(r);
+    console.log(r);
+    console.log(r);
+    console.log(r);
+    return song;
+}
+
+song();
