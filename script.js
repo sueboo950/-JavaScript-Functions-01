de18a4a030eb28789899da11c962f48f1a35f8af
sophie950 <sueboo950950@gmail.com>
2024-08-06 23:28:02 -0500

    completed day 4 song and day 1 week 2 assignment

---
+++ script.js
@@ -0,0 +1,59 @@
+function refrain(){
+    let words = "";
+    words += "We magnify Your name\n";
+    words += "Glorify Your name\n";
+    words += "Oh God\n";
+    return words;
+}
+
+
+function song(){
+    let r;
+    let Sword = "";
+
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Forever You are the same\n");
+    Swords += ("We worship and adore You\n");
+    Swords += ("We bow ourselves before You\n");
+    Swords += ("Giving You the glory\n");
+    Swords += ("That is due Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Forever You are the same\n");
+    Swords += ("We worship and adore You\n");
+    Swords += ("We bow ourselves before You\n");
+    Swords += ("Giving You the glory\n");
+    Swords += ("That is due Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Glory to Your name\n");
+    Swords += ("Forever You are the same\n");
+    Swords += ("We worship and adore You\n");
+    Swords += ("We bow ourselves before You\n");
+    Swords += ("Giving You the glory\n");
+    Swords += ("That is due Your name\n");
+    r = refrain();
+    Swords += (r);
+    Swords += (r);
+    Swords += (r);
+    Swords += (r);
+    Swords += (r);
+    Swords += (r);
+    return Swords;
+}
+
+song();
+
+let tag = document.getElementById("yourName");
+
+function handleClick() {
+    let songLyrics;
+    songLyrics = song();
+    tag.innerHTML = songLyrics;
+}
+
+let button = document.getElementById("showLyrics");
+button.addEventListener("click", handleClick);
